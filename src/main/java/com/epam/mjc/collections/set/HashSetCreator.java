87815a26b34eb6a6d1bb449ed41b84c71ae07589
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-05 20:35:00 +0400

    Update HashSetCreator.java

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> oddEven = new HashSet<>();
+        for (Integer k : sourceList) {
+            Integer n = k;
+            while (n%2 == 0){
+                oddEven.add(n);
+                oddEven.add(n/2);
+                n /= 2;
+            }
+            if(k %2 != 0){
+                oddEven.add(k);
+                oddEven.add(k * 2);
+            }
+        }
+        return oddEven;
     }
 }
